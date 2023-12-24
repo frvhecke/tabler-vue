8b6af35ed825f5686adc430c9798404834cd2dab
frvhecke <frederik@cluttr.be>
2023-12-24 11:53:46 +0100

    Updating router

--- src/router/index.js
+++ src/router/index.js
@@ -14,6 +14,56 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+  },
+  {
+    path: '/layout/horizontal',
+    name: 'Horizontal Layout',
+    component: () => import('../views/layout/LayoutHorizontal.vue')
+  },
+  {
+    path: '/layout/combined',
+    name: 'Combined Layout',
+    component: () => import('../views/layout/LayoutCombined.vue')
+  },
+  {
+    path: '/layout/vertical',
+    name: 'Vertical Layout',
+    component: () => import('../views/layout/LayoutVertical.vue')
+  },
+  {
+    path: '/layout/vertical-transparent',
+    name: 'Vertical Transparent Layout',
+    component: () => import('../views/layout/LayoutVerticalTransparent.vue')
+  },
+  {
+    path: '/layout/navbar-dark',
+    name: 'NavBar Dark Layout',
+    component: () => import('../views/layout/LayoutNavBarDark.vue')
+  },
+  {
+    path: '/interface/accordion',
+    name: 'Accordion',
+    component: () => import('../views/interface/AccordionPage.vue')
+  },
+  {
+    path: '/interface/buttons',
+    name: 'Buttons',
+    component: () => import('../views/interface/ButtonsPage.vue')
+  },
+  {
+    path: '/interface/cards',
+    name: 'Cards',
+    component: () => import('../views/interface/CardsPage.vue')
+  },
+  {
+    path: '/interface/colors',
+    name: 'Colors',
+    component: () => import('../views/interface/ColorsPage.vue')
+  },
+  {
+    path: '/interface/form',
+    name: 'Form Elements',
+    component: () => import('../views/interface/FormPage.vue')
   }
 ]
 
