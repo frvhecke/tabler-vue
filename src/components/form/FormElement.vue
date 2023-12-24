9f4768893f942f7c44039b90babfe31890a084e4
frvhecke <frederik@cluttr.be>
2023-12-24 11:52:24 +0100

    Adding form components

---
+++ src/components/form/FormElement.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="mb-3" :class="[ { 'row' : horizontal } ]">
+    <FormLabel v-if="label" :text="label" :required="required" :description="description" :class="[ { 'col-3 col-form-label' : horizontal } ]"/>
+    <div :class="[ { 'col' : horizontal } ]"><slot></slot></div>
+  </div>
+</template>
+
+<script>
+import FormLabel from '@/components/form/FormLabel.vue'
+
+export default {
+  name: 'FormElement',
+  components: {
+    FormLabel
+  },
+  props: {
+    label: { default: '', type: String },
+    description: { default: '', type: String },
+    required: { default: false, type: Boolean },
+    horizontal: { default: false, type: Boolean },
+  }
+}
+</script>
