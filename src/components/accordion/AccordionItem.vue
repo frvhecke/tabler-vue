6e0b372db895dcbe0788d34f61c1fc2068d0903a
frvhecke <frederik@cluttr.be>
2023-12-24 11:52:59 +0100

    Adding accordion components

---
+++ src/components/accordion/AccordionItem.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="accordion-item">
+    <h2 class="accordion-header">
+      <button class="accordion-button" type="button" :aria-expanded="collapsed">
+        {{ title }}
+      </button>
+    </h2>
+    <div id="collapse-1" class="accordion-collapse collapse" :class="[ { 'show' : !collapsed } ]">
+      <div class="accordion-body pt-0">
+        <slot></slot>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AccordionItem',
+  components: {
+  },
+  props: {
+    title: { default: '', type: String },
+  },
+  data: () => ({
+    collapsed: false
+  }),
+  computed: {
+  },
+  methods: {
+  }
+}
+</script>
---
+++ src/components/accordion/AccordionWrapper.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="accordion">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AccordionWrapper'
+}
+</script>
