732fc3d54d4e1051625ff5b3fad975b886386013
frvhecke <frederik@cluttr.be>
2023-12-24 11:51:16 +0100

    Adding icon component

---
+++ src/components/icon/TablerIcon.vue
@@ -0,0 +1,29 @@
+<template>
+  <i class="icon" :class="[ getIconName(), getColor() ]" :style="{ 'font-size': size_style }"></i>
+</template>
+
+<script>
+import { getTextColor } from '@/services/color/ColorUtils.js'
+
+export default {
+  name: 'TablerIcon',
+  props: {
+    icon: { default: '', type: String },
+    color: { default: '', type: String },
+    size: { default: -1, type: Number }
+  },
+  computed: {
+    size_style () {
+      return (this.size !== -1) ? this.size+'px' : ''
+    }
+  },
+  methods: {
+    getIconName() {
+      return 'ti ti-'+this.icon
+    },
+    getColor () {
+      return getTextColor(this.color)
+    }
+  }
+}
+</script>
