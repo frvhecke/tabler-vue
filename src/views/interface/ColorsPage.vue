6ea6003a60baedda28c1bd209cd62d41c2e20d80
frvhecke <frederik@cluttr.be>
2023-12-24 11:54:28 +0100

    Adding colors page

---
+++ src/views/interface/ColorsPage.vue
@@ -0,0 +1,315 @@
+<template>
+  <PageContainer>
+    <NavBar />
+    <PageWrapper>
+      <PageTitle title="Colors" />
+      <PageBody>
+        <SimpleCard>
+          <ResponsiveTable center>
+            <TableRow>
+              <TableCell>
+                <TextAvatar text="BL" text_color="blue-fg" background_color="blue"/>
+              </TableCell>
+              <TableCell>
+                <TextAvatar text="AZ" text_color="azure-fg" background_color="azure"/>
+              </TableCell>
+              <TableCell>
+                <TextAvatar text="IN" text_color="indigo-fg" background_color="indigo"/>
+              </TableCell>
+              <TableCell>
+                <TextAvatar text="PU" text_color="purple-fg" background_color="purple"/>
+              </TableCell>
+              <TableCell>
+                <TextAvatar text="PI" text_color="pink-fg" background_color="pink"/>
+              </TableCell>
+              <TableCell>
+                <TextAvatar text="RE" text_color="red-fg" background_color="red"/>
+              </TableCell>
+              <TableCell>
+                <TextAvatar text="OR" text_color="orange-fg" background_color="orange"/>
+              </TableCell>
+              <TableCell>
+                <TextAvatar text="YE" text_color="yellow-fg" background_color="yellow"/>
+              </TableCell>
+              <TableCell>
+                <TextAvatar text="LI" text_color="lime-fg" background_color="lime"/>
+              </TableCell>
+              <TableCell>
+                <TextAvatar text="GR" text_color="green-fg" background_color="green"/>
+              </TableCell>
+              <TableCell>
+                <TextAvatar text="TE" text_color="teal-fg" background_color="teal"/>
+              </TableCell>
+              <TableCell>
+                <TextAvatar text="CY" text_color="cyan-fg" background_color="cyan"/>
+              </TableCell>
+              <TableCell>
+                <TextAvatar text="DA" text_color="dark-fg" background_color="dark"/>
+              </TableCell>
+              <TableCell>
+                <TextAvatar text="MU" text_color="muted-fg" background_color="muted"/>
+              </TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell>
+                <TextAvatar text="BL" background_color="blue-lt"/>
+              </TableCell>
+              <TableCell>
+                <TextAvatar text="AZ" background_color="azure-lt"/>
+              </TableCell>
+              <TableCell>
+                <TextAvatar text="IN" background_color="indigo-lt"/>
+              </TableCell>
+              <TableCell>
+                <TextAvatar text="PU" background_color="purple-lt"/>
+              </TableCell>
+              <TableCell>
+                <TextAvatar text="PI" background_color="pink-lt"/>
+              </TableCell>
+              <TableCell>
+                <TextAvatar text="RE" background_color="red-lt"/>
+              </TableCell>
+              <TableCell>
+                <TextAvatar text="OR" background_color="orange-lt"/>
+              </TableCell>
+              <TableCell>
+                <TextAvatar text="YE" background_color="yellow-lt"/>
+              </TableCell>
+              <TableCell>
+                <TextAvatar text="LI" background_color="lime-lt"/>
+              </TableCell>
+              <TableCell>
+                <TextAvatar text="GR" background_color="green-lt"/>
+              </TableCell>
+              <TableCell>
+                <TextAvatar text="TE" background_color="teal-lt"/>
+              </TableCell>
+              <TableCell>
+                <TextAvatar text="CY" background_color="cyan-lt"/>
+              </TableCell>
+              <TableCell>
+                <TextAvatar text="DA" background_color="dark-lt"/>
+              </TableCell>
+              <TableCell>
+                <TextAvatar text="MU" background_color="muted-lt"/>
+              </TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell>
+                <TextAvatar text="BL" text_color="blue" transparent />
+              </TableCell>
+              <TableCell>
+                <TextAvatar text="AZ" text_color="azure" transparent />
+              </TableCell>
+              <TableCell>
+                <TextAvatar text="IN" text_color="indigo" transparent />
+              </TableCell>
+              <TableCell>
+                <TextAvatar text="PU" text_color="purple" transparent />
+              </TableCell>
+              <TableCell>
+                <TextAvatar text="PI" text_color="pink" transparent />
+              </TableCell>
+              <TableCell>
+                <TextAvatar text="RE" text_color="red" transparent />
+              </TableCell>
+              <TableCell>
+                <TextAvatar text="OR" text_color="orange" transparent />
+              </TableCell>
+              <TableCell>
+                <TextAvatar text="YE" text_color="yellow" transparent />
+              </TableCell>
+              <TableCell>
+                <TextAvatar text="LI" text_color="lime" transparent />
+              </TableCell>
+              <TableCell>
+                <TextAvatar text="GR" text_color="green" transparent />
+              </TableCell>
+              <TableCell>
+                <TextAvatar text="TE" text_color="teal" transparent />
+              </TableCell>
+              <TableCell>
+                <TextAvatar text="CY" text_color="cyan" transparent />
+              </TableCell>
+              <TableCell>
+                <TextAvatar text="DA" text_color="dark" transparent />
+              </TableCell>
+              <TableCell>
+                <TextAvatar text="MU" text_color="muted" transparent />
+              </TableCell>
+            </TableRow>
+            <TableRow background_color="light">
+              <TableCell>
+                <TextAvatar text="BL" text_color="blue" transparent />
+              </TableCell>
+              <TableCell>
+                <TextAvatar text="AZ" text_color="azure" transparent />
+              </TableCell>
+              <TableCell>
+                <TextAvatar text="IN" text_color="indigo" transparent />
+              </TableCell>
+              <TableCell>
+                <TextAvatar text="PU" text_color="purple" transparent />
+              </TableCell>
+              <TableCell>
+                <TextAvatar text="PI" text_color="pink" transparent />
+              </TableCell>
+              <TableCell>
+                <TextAvatar text="RE" text_color="red" transparent />
+              </TableCell>
+              <TableCell>
+                <TextAvatar text="OR" text_color="orange" transparent />
+              </TableCell>
+              <TableCell>
+                <TextAvatar text="YE" text_color="yellow" transparent />
+              </TableCell>
+              <TableCell>
+                <TextAvatar text="LI" text_color="lime" transparent />
+              </TableCell>
+              <TableCell>
+                <TextAvatar text="GR" text_color="green" transparent />
+              </TableCell>
+              <TableCell>
+                <TextAvatar text="TE" text_color="teal" transparent />
+              </TableCell>
+              <TableCell>
+                <TextAvatar text="CY" text_color="cyan" transparent />
+              </TableCell>
+              <TableCell>
+                <TextAvatar text="DA" text_color="dark" transparent />
+              </TableCell>
+              <TableCell>
+                <TextAvatar text="MU" text_color="muted" transparent />
+              </TableCell>
+            </TableRow>
+            <TableRow text_color="white" background_color="dark">
+              <TableCell>
+                <TextAvatar text="BL" text_color="blue" transparent />
+              </TableCell>
+              <TableCell>
+                <TextAvatar text="AZ" text_color="azure" transparent />
+              </TableCell>
+              <TableCell>
+                <TextAvatar text="IN" text_color="indigo" transparent />
+              </TableCell>
+              <TableCell>
+                <TextAvatar text="PU" text_color="purple" transparent />
+              </TableCell>
+              <TableCell>
+                <TextAvatar text="PI" text_color="pink" transparent />
+              </TableCell>
+              <TableCell>
+                <TextAvatar text="RE" text_color="red" transparent />
+              </TableCell>
+              <TableCell>
+                <TextAvatar text="OR" text_color="orange" transparent />
+              </TableCell>
+              <TableCell>
+                <TextAvatar text="YE" text_color="yellow" transparent />
+              </TableCell>
+              <TableCell>
+                <TextAvatar text="LI" text_color="lime" transparent />
+              </TableCell>
+              <TableCell>
+                <TextAvatar text="GR" text_color="green" transparent />
+              </TableCell>
+              <TableCell>
+                <TextAvatar text="TE" text_color="teal" transparent />
+              </TableCell>
+              <TableCell>
+                <TextAvatar text="CY" text_color="cyan" transparent />
+              </TableCell>
+              <TableCell>
+                <TextAvatar text="WH" text_color="white" transparent />
+              </TableCell>
+              <TableCell>
+                <TextAvatar text="MU" text_color="muted" transparent />
+              </TableCell>
+            </TableRow>
+            <TableRow text_color="white" background_color="dark">
+              <TableCell>
+                <TextAvatar text="BL" background_color="blue-lt"/>
+              </TableCell>
+              <TableCell>
+                <TextAvatar text="AZ" background_color="azure-lt"/>
+              </TableCell>
+              <TableCell>
+                <TextAvatar text="IN" background_color="indigo-lt"/>
+              </TableCell>
+              <TableCell>
+                <TextAvatar text="PU" background_color="purple-lt"/>
+              </TableCell>
+              <TableCell>
+                <TextAvatar text="PI" background_color="pink-lt"/>
+              </TableCell>
+              <TableCell>
+                <TextAvatar text="RE" background_color="red-lt"/>
+              </TableCell>
+              <TableCell>
+                <TextAvatar text="OR" background_color="orange-lt"/>
+              </TableCell>
+              <TableCell>
+                <TextAvatar text="YE" background_color="yellow-lt"/>
+              </TableCell>
+              <TableCell>
+                <TextAvatar text="LI" background_color="lime-lt"/>
+              </TableCell>
+              <TableCell>
+                <TextAvatar text="GR" background_color="green-lt"/>
+              </TableCell>
+              <TableCell>
+                <TextAvatar text="TE" background_color="teal-lt"/>
+              </TableCell>
+              <TableCell>
+                <TextAvatar text="CY" background_color="cyan-lt"/>
+              </TableCell>
+              <TableCell>
+                <TextAvatar text="WH" background_color="white-lt"/>
+              </TableCell>
+              <TableCell>
+                <TextAvatar text="MU" background_color="muted-lt"/>
+              </TableCell>
+            </TableRow>
+          </ResponsiveTable>
+        </SimpleCard>
+      </PageBody>
+    </PageWrapper>
+  </PageContainer>
+</template>
+
+<script>
+import PageContainer from '@/components/page/PageContainer.vue'
+import NavBar from '@/components/navigation/NavBar.vue'
+import PageWrapper from '@/components/page/PageWrapper.vue'
+import PageTitle from '@/components/page/PageTitle.vue'
+import PageBody from '@/components/page/PageBody.vue'
+import SimpleCard from '@/components/card/SimpleCard.vue'
+import ResponsiveTable from '@/components/table/ResponsiveTable.vue'
+import TableRow from '@/components/table/TableRow.vue'
+import TableCell from '@/components/table/TableCell.vue'
+import TextAvatar from '@/components/avatar/TextAvatar.vue'
+
+export default {
+  name: 'ColorsPage',
+  components: {
+    PageContainer,
+    NavBar,
+    PageWrapper,
+    PageTitle,
+    PageBody,
+    SimpleCard,
+    ResponsiveTable,
+    TableRow,
+    TableCell,
+    TextAvatar,
+  },
+  props: {
+  },
+  data: () => ({
+  }),
+  computed: {
+  },
+  methods: {
+  }
+}
+</script>
