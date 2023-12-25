e7c2ce5070bc1af2adc0a1cbd5cecfb3b63b28b7
frvhecke <frederik@cluttr.be>
2023-12-25 23:45:09 +0100

    Adding select input component

---
+++ src/components/form/input/SelectInput.vue
@@ -0,0 +1,56 @@
+<template>
+  <select class="form-select" @input="update($event.target.value)">
+    <option
+      v-for="item in values"
+      :key="item.value"
+      :value="item.value"
+      :selected="selected(item.value)"
+    >
+      {{ item.text }}
+    </option>
+  </select>
+  <small v-if="hint" class="form-hint" :id="describedby()">{{ hint }}</small>
+  <div v-if="feedback" class="invalid-feedback">{{ feedback }}</div>
+</template>
+
+<script>
+export default {
+  name: 'SelectInput',
+  props: {
+    placeholder: { default: '', type: String },
+    name: { default: 'text', type: String },
+    values: { default() { return {} }, type: Object },
+    modelValue: { default: '', type: String },
+    disabled: { default: false, type: Boolean },
+    readonly: { default: false, type: Boolean },
+    valid: { default: false, type: Boolean },
+    invalid: { default: false, type: Boolean },
+    lite: { default: false, type: Boolean },
+    hint: { default: '', type: String },
+    feedback: { default: '', type: String },
+    mb2: { default: false, type: Boolean },
+    mb3: { default: false, type: Boolean },
+  },
+  data: () => ({
+    value: ''
+  }),
+  methods: {
+    update ($value) {
+      this.value = $value
+      this.$emit('update:modelValue', this.value)
+    },
+    describedby() {
+      if(this.hint) return this.name + '_help'
+      else return null
+    },
+    selected ($value) {
+      if(this.value === $value) return true
+      else return null
+    }
+  },
+  mounted() {
+    this.value = this.modelValue
+    this.$emit('update:modelValue', this.value)
+  }
+}
+</script>
