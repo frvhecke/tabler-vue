1cca2b5bf44a64f5c122e094b5b9f133ae8c5494
frvhecke <frederik@cluttr.be>
2023-12-27 00:18:02 +0100

    Adding input group component

---
+++ src/components/form/input/InputGroup.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="input-group" :class="[ { 'mb-2' : mb2 }, { 'mb-3' : mb3 } ]">
+    <span v-if="text_left" class="input-group-text">
+      {{ text_left }}
+    </span>
+    <slot></slot>
+    <span v-if="text_right" class="input-group-text">
+      {{ text_right }}
+    </span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'InputGroup',
+  props: {
+    text_left: { default: '', type: String},
+    text_right: { default: '', type: String},
+    mb2: { default: false, type: Boolean },
+    mb3: { default: false, type: Boolean },
+  }
+}
+</script>
